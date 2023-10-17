583e9b46d7559c2290667def81447f3f0b7d6ca7
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-17 22:29:47 +0530

    chat message components

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { auth } from '../config/firebase';
+
+function ChatMessage(props) {
+    const { text, uid, photoURL } = props.message;
+  
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+  
+    return (<>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} 
+        alt='https://api.adorable.io/avatars/23/abott@adorable.png'/>
+        <p>{text}</p>
+      </div>
+    </>)
+  }
+  
+
+export default ChatMessage
