b05097c3e74d1597afc77d258122dd56930bbd9f
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-17 22:31:26 +0530

    side bar not completed yet

---
+++ src/components/SideBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function SideBar() {
+  return (
+    <div className='sidebar'>
+        <h2>User name</h2>
+        {/* a line */}
+        {/* reate room   */}
+        <>
+        <h4>Ccrat</h4><div>+</div>
+        {/* list */}
+        <ul>
+            <li>room 1</li>
+        </ul>
+        </>
+
+    </div>
+  )
+}
+
+export default SideBar
