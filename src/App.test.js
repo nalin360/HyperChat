3e3d518c368f54681d930b4a5c7d315d2def51b3
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-17 22:35:09 +0530

    updated react 16 to 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+// });
+
+// const { mockCollection } = require('firestore-jest-mock/mocks/firestore');
+// test('testing stuff', () => {
+//   const firebase = require('firebase'); // or import firebase from 'firebase';
+//   const db = firebase.firestore();
+//   return db;
+// })
