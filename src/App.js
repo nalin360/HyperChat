c6cd8cc615c2f9304758bf124b3c693cd81c79bd
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-17 22:34:06 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,122 +1,68 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+// import firebase from 'firebase/app';
+// import { collection, orderBy, query, addDoc, serverTimestamp, getDocs } from "firebase/firestore"
+
 import 'firebase/auth';
 import 'firebase/analytics';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  // your config
-})
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-const analytics = firebase.analytics();
+import { auth } from './config/firebase';
+import SignOut from './components/SignOut';
+import LoadContainer from './components/LoadContainer';
+// import SideBar from './components/SideBar';
 
 
 function App() {
 
-  const [user] = useAuthState(auth);
+  // const userPhotoURL = auth.currentUser.photoURL;
+  const [user, setUser] = useState(false);
+  // const [isSidebar, setisSidebar] = useState(false)
+  // const fordev = true
+  useEffect(() => {
+    // Use the `onAuthStateChanged` method to handle authentication state
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser != null) {
+        setUser(true);
+      } else {
+        setUser(false);
+      }
+      // Once the authentication state has been checked, set loading to false
+      // setLoading(false);
+    });
+    // Unsubscribe when the component unmounts to prevent memory leaks
+    return () => unsubscribe();
+  }, []);
 
+  // const handelSidebar = () => {
+  //   if (isSidebar === false) {
+  //     // console.log(isSidebar);
+  //     setisSidebar(true)
+  //   } else {
+  //     setisSidebar(false)
+  //   }
+  // }
   return (
     <div className="App">
+
       <header>
-        <h1>⚛️🔥💬</h1>
+        {/* <button className='user-icon'
+          onClick={handelSidebar}> */}
+            
+            {/* <img src={userPhotoURL  || 'https://api.adorable.io/avatars/23/abott@adorable.png'}
+          alt='https://api.adorable.io/avatars/23/abott@adorable.png' /> */}
+        {/* </button> */}
+        <h1>HyperChat</h1>
         <SignOut />
       </header>
-
+      {/* {console.log(isSidebar)} */}
+      {/* {isSidebar ? <SideBar /> : <></>} */}
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        <LoadContainer isuser={user} />
       </section>
-
     </div>
   );
-}
-
-function SignIn() {
-
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return (
-    <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
-    </>
-  )
-
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
-}
-
-
-function ChatRoom() {
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
-
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-
-    const { uid, photoURL } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid,
-      photoURL
-    })
-
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
-
-  return (<>
-    <main>
-
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-
-      <span ref={dummy}></span>
-
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
-}
-
-
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
 }
 
 
